ea0de9fde7e1e6d42aa871291022ccecc5f2e899
Ezequiel <ezequiel@MacBook-Pro-de-Ezequiel.local>
2022-12-16 16:59:25 -0300

    First commit

---
+++ src/data/tasks.js
@@ -0,0 +1,20 @@
+export const tasks = [
+    {
+       id: 0,
+       title: 'Mi primer tarea',
+       description: 'Descripcion mi primer tarea'
+
+    },
+    {
+        id: 1,
+        title: 'Mi segunda tarea',
+        description: 'Descripcion de mi segunda tarea'
+ 
+     },
+     {
+        id: 2,
+        title: 'Mi tercer tarea',
+        description: 'Descripcion de mi tercer tarea'
+ 
+     }
+]
