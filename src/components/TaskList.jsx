e657b32b51f6d40c4fbc18b2bc0fb3de8b252539
Ezequiel Duré <106278375+ezequieldureit@users.noreply.github.com>
2022-12-28 11:13:05 -0300

    Update TaskList.jsx

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,7 +6,7 @@ import { useContext } from "react";
 
   const {tasks} = useContext(TaskContext)
   if (tasks.length === 0) {
-    return <h1 className="text-white text-4xl font-bold text-center">No hay tareas aun</h1>;
+    return <h1 className="text-white text-4xl font-bold text-center">No task yet</h1>;
   }
   return (
     <div className="grid grid-cols-4 gap-2">
