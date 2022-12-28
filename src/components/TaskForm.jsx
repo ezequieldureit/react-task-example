6ed18d1eea8e13352e21abab1f75a602494eee48
Ezequiel Duré <106278375+ezequieldureit@users.noreply.github.com>
2022-12-28 11:17:08 -0300

    Update TaskForm.jsx

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -18,7 +18,7 @@ function TaskForm() {
   return (
     <div className="max-w-md mx-auto">
       <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4" >
-      <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
+      <h1 className="text-2xl font-bold text-white mb-3">Create your task</h1>
       <input
         placeholder="Enter your task"
         onChange={(event) => setTitle(event.target.value)}
@@ -32,7 +32,7 @@ function TaskForm() {
         value={description}
         className="bg-slate-300 p-3 w-full mb-2" autoFocus
       ></textarea>
-      <button className="bg-indigo-500 px-3 py-1 text-white ">Guardar</button>
+      <button className="bg-indigo-500 px-3 py-1 text-white ">Save</button>
     </form>
     </div>
   );
